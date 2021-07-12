cb4b4d25215c5f331e054cd91eab5e030fe37e80
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2021-07-12 13:08:46 +0200

    add todos with form and storage

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-let number = 0
-
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    number = number + 1
-    const formData = new FormData(e.target);
-    const search = formData.get("search")
-    chrome.storage.sync.set({[number]: search}, function () {
-    });
-
-    console.log("La valeur rentrée est " + search)
-    console.log("Number est égale à " + number)
-})
-
 const deleteButton = document.querySelector("#delete")
 deleteButton.onclick = () => {
     chrome.storage.sync.clear()
@@ -34,10 +20,42 @@ chrome.storage.sync.get(null, function (data) {
                 <div class="todos-right-image-wrapper">
                     <div class="todos-image-wrapper"></div>
                 </div>
-                <div class="todos-right-main-wrapper"></div>
+                <div class="todos-right-main-wrapper">
+                    <h1 class="todos-h1">${data[i]}</h1>
+                </div>
                 <div class="todos-right-time-wrapper"></div>
             </div>
         </div>
         `
     }
 });
+
+/////////////////////////////////////////////// ADD TODOS  ///////////////////////////////////////////////////
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    chrome.storage.sync.get(null, function (data) {
+        let number = Object.keys(data).length + 1
+        console.log(number)
+        const formData = new FormData(e.target);
+        const search = formData.get("search")
+        chrome.storage.sync.set({[number]: search}, function () {
+            todosWrapper.innerHTML += `
+        <div class="todos-wrapper">
+            <div class="todos-left-wrapper">
+                <input type="checkbox" class="todos-checkbox">
+            </div>
+            <div class="todos-right-wrapper">
+                <div class="todos-right-image-wrapper">
+                    <div class="todos-image-wrapper"></div>
+                </div>
+                <div class="todos-right-main-wrapper">
+                    <h1 class="todos-h1">${search}</h1>
+                </div>
+                <div class="todos-right-time-wrapper"></div>
+            </div>
+        </div>
+        `
+        });
+    })
+})

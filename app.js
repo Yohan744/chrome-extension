6b2483bd2771758a19f04061f5e75aa79fb903d4
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2021-07-24 16:08:15 +0200

    add possibility to delete to do, and to add with right number

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ deleteButton.onclick = () => {
 const todosWrapper = document.querySelector("#todos-global-wrapper")
 
 chrome.storage.sync.get(null, function (data) {
-    console.log(data)
     for (let i = 1; i < Object.keys(data).length + 1; i++) {
         todosWrapper.innerHTML += `
-        <div class="todos-wrapper">
+        <div class="todos-wrapper ${i}">
             <div class="todos-left-wrapper">
                 <input type="checkbox" class="todos-checkbox">
             </div>
@@ -35,27 +34,43 @@ chrome.storage.sync.get(null, function (data) {
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     chrome.storage.sync.get(null, function (data) {
-        let number = Object.keys(data).length + 1
-        console.log(number)
+        console.log(data)
+        let count = Object.keys(data).length - 1
+        let number = Object.keys(data)[count]
         const formData = new FormData(e.target);
         const search = formData.get("search")
-        chrome.storage.sync.set({[number]: search}, function () {
+        chrome.storage.sync.set({[number + 1]: search}, function () {
             todosWrapper.innerHTML += `
-        <div class="todos-wrapper">
-            <div class="todos-left-wrapper">
-                <input type="checkbox" class="todos-checkbox">
-            </div>
-            <div class="todos-right-wrapper">
-                <div class="todos-right-image-wrapper">
-                    <div class="todos-image-wrapper"></div>
+            <div class="todos-wrapper ${number}">
+                <div class="todos-left-wrapper">
+                    <input type="checkbox" class="todos-checkbox">
                 </div>
-                <div class="todos-right-main-wrapper">
-                    <h1 class="todos-h1">${search}</h1>
+                <div class="todos-right-wrapper">
+                    <div class="todos-right-image-wrapper">
+                        <div class="todos-image-wrapper"></div>
+                    </div>
+                    <div class="todos-right-main-wrapper">
+                        <h1 class="todos-h1">${search}</h1>
+                    </div>
+                    <div class="todos-right-time-wrapper"></div>
                 </div>
-                <div class="todos-right-time-wrapper"></div>
             </div>
-        </div>
         `
         });
     })
+})
+
+/////////////////////////////////////////////// DELETE TODOS ///////////////////////////////////////////////////
+
+
+document.addEventListener("click", e => {
+    if (e.target.classList.contains("todos-checkbox")){
+        let todosCount = e.target.parentElement.parentElement.className.split(' ')
+        console.log("la div a comme classe " + todosCount[1])
+        e.target.parentElement.parentElement.remove()
+        chrome.storage.sync.remove(todosCount[1], function (){})
+        chrome.storage.sync.get(null, function (data) {
+            console.log(data)
+        })
+    }
 })

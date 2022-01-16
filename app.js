c69b4067c63331471a290d6f8ee8094e8a47e905
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2022-01-16 19:30:11 +0100

    add other.js, images and lot of cool stuff, still need to work on the date section

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 ////////////////////////////////////////// GET ALL TODOS ///////////////////////////////////////////////
 
+let today = new Date().toISOString().slice(0, 10)
 
 const todosWrapper = document.querySelector("#todos-global-wrapper")
 
 chrome.storage.sync.get(null, function (data) {
+
     for (let i = 0; i < Object.keys(data).length ; i++) {
+
+        console.log(data)
+
+        if (Object.values(data)[i].date === today) {
+
+        }
+
         todosWrapper.innerHTML += `
         <div class="todos-wrapper ${Object.keys(data)[i]}">
             <div class="todos-left-wrapper">
@@ -12,60 +21,112 @@ chrome.storage.sync.get(null, function (data) {
             </div>
             <div class="todos-right-wrapper">
                 <div class="todos-right-image-wrapper">
-                    <div class="todos-image-wrapper"></div>
+                    <div class="todos-image-wrapper" style="background-color: ${Object.values(data)[i].color}">
+                        <img src="image/${Object.values(data)[i].category}.png" alt="image">
+                    </div>
                 </div>
                 <div class="todos-right-main-wrapper">
-                    <h1 class="todos-h1">${Object.values(data)[i]}</h1>
+                    <h1 class="todos-h1">${Object.values(data)[i].task}</h1>
                 </div>
-                <div class="todos-right-time-wrapper"></div>
+                <div class="todos-right-time-wrapper">
+                    <p class="todos-time">${Object.values(data)[i].time}</p>
+                </div>
             </div>
         </div>
         `
+
     }
+
 });
-
-
-////////////////////////////////////////// DELETE ALL TODOS ///////////////////////////////////////////////
-
-
-const deleteButton = document.querySelector("#delete")
-deleteButton.onclick = () => {
-    chrome.storage.sync.clear()
-    todosWrapper.innerHTML = `<div class="blankTodo 0"></div>`
-}
 
 
 /////////////////////////////////////////////// ADD TODOS  ///////////////////////////////////////////////////
 
+const searchInputButton = document.querySelector("#searchInput")
 
-document.querySelector('form').addEventListener('submit', (e) => {
+searchInputButton.onclick = () => {
+    searchInputButton.addEventListener("keypress", (event)=> {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+        }
+    });
+}
+
+document.querySelector("#dateInput").valueAsDate = new Date()
+document.querySelector("#dateInput").min = new Date()
+
+const categoriesWrapper = document.querySelector(".all-categories-wrapper")
+let category
+let elementColor
+let verifCategory = false
+categoriesWrapper.addEventListener("click", e => {
+    category = e.target.innerText
+    let element = getComputedStyle(e.target)
+    elementColor= element.backgroundColor
+
+    verifCategory = true
+
+    let categoriesWrapperChild = categoriesWrapper.querySelector(".active");
+
+    if (categoriesWrapperChild != null) {
+        categoriesWrapperChild.classList.remove('active')
+    }
+
+    e.target.classList.add("active")
+
+})
+
+const createTaskButton = document.querySelector("#createTaskButton")
+
+createTaskButton.addEventListener('click', (e) => {
+
+    let search = document.querySelector("#searchInput").value
+    let date = document.querySelector("#dateInput").value
+    let time = document.querySelector("#timeInput").value
+
+    let curentData = {"task": search,"date": date,"time": time,"category": category, "color": elementColor}
+
     e.preventDefault();
-    chrome.storage.sync.get(null, function (data) {
-        let getLastChild = document.querySelector("#todos-global-wrapper").lastElementChild
-        console.log(getLastChild)
-        let number = Number(getLastChild.classList[1]) + 1
-        const formData = new FormData(e.target);
-        const search = formData.get("search")
-        chrome.storage.sync.set({[number]: search}, function () {
-            todosWrapper.innerHTML += `
+
+    if ((search !== "") && (verifCategory === true)) {
+        chrome.storage.sync.get(null, function (data) {
+            let getLastChild = document.querySelector("#todos-global-wrapper").lastElementChild
+            let number = Number(getLastChild.classList[1]) + 1
+            chrome.storage.sync.set({[number]: curentData}, function () {
+                todosWrapper.innerHTML += `
             <div class="todos-wrapper ${number}">
                 <div class="todos-left-wrapper">
                     <input type="checkbox" class="todos-checkbox">
                 </div>
                 <div class="todos-right-wrapper">
                     <div class="todos-right-image-wrapper">
-                        <div class="todos-image-wrapper"></div>
+                        <div class="todos-image-wrapper" style="background-color: ${elementColor}">
+                            <img src="image/${category}.png" alt="image">
+                        </div>
                     </div>
                     <div class="todos-right-main-wrapper">
-                        <h1 class="todos-h1">${search}</h1>
+                        <h1 class="todos-h1">${curentData.task}</h1>
                     </div>
-                    <div class="todos-right-time-wrapper"></div>
+                    <div class="todos-right-time-wrapper">
+                        <p class="todos-time">${curentData.time}</p>
+                    </div>
                 </div>
             </div>
         `
-        });
+            });
+            console.log(data)
+        })
 
-    })
+        switchBetweenSections()
+
+        verifCategory = false
+
+        cleanAddSection()
+
+    }
+
+
+
 })
 
 /////////////////////////////////////////////// DELETE TODOS ///////////////////////////////////////////////////
@@ -79,38 +140,3 @@ document.addEventListener("click", e => {
         })
     }
 })
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////// Date /////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-let actualDay = new Date().getDay()
-let dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-
-let actualDate = new Date().getDate()
-console.log("Nous sommes le " + dayOfTheWeek[actualDay] + " " + actualDate)
-
-const titleWrapper = document.querySelector(".main-title-wrapper")
-titleWrapper.innerHTML += `<p>${dayOfTheWeek[actualDay] + " " + actualDate}</p>`
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////// Switch between sections  ///////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// main -> add
-const addTodoButton = document.querySelector("#add-wrapper")
-const mainGlobalWrapper = document.querySelector("#main-global-wrapper")
-const AddGlobalWrapper = document.querySelector("#add-global-wrapper")
-
-addTodoButton.onclick = () => {
-    mainGlobalWrapper.classList.toggle("active")
-    AddGlobalWrapper.classList.toggle("active")
-}
-
-// add -> main
-const backArrow = document.querySelector(".back-arrow-wrapper")
-
-backArrow.onclick = () => {
-    AddGlobalWrapper.classList.toggle("active")
-    mainGlobalWrapper.classList.toggle("active")
-}

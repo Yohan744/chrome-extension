a4f779932ec28aa719d564d5b2641ce51e988cb2
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2023-02-20 19:23:09 +0100

    new logo and new fix

---
+++ js/app.js
@@ -0,0 +1,194 @@
+////////////////////////////////////////// GET ALL TODOS ///////////////////////////////////////////////
+
+const todosWrapper = document.querySelector("#todos-global-wrapper")
+
+// chrome.storage.sync.clear()
+
+chrome.storage.sync.get(null, function (data) {
+
+    for (let i = 0; i < Object.keys(data).length; i++) {
+
+        todosWrapper.innerHTML += `
+        <div class="todos-wrapper ${Object.keys(data)[i]}">
+            <label class="todos-checkbox">
+                <input class="checkbox_input" type="checkbox">
+                <svg class="checkbox_check" width="25" height="25">
+                    <polyline points="16 3 7 15 2 10"></polyline>
+                </svg>
+            </label>
+            <div class="todos-right-wrapper">
+                <div class="todos-image-wrapper" style="background-color: ${Object.values(data)[i].color}">
+                    <img src="image/${Object.values(data)[i].category}.png" alt="image">
+                </div>
+                <div class="todos-right-main-wrapper">
+                    <h1 class="todos-h1">${Object.values(data)[i].task}</h1>
+                </div>
+                <div class="todos-right-color-wrapper" style="background-color: ${Object.values(data)[i].color}"></div>
+            </div>
+        </div>
+        `
+
+    }
+
+});
+
+
+/////////////////////////////////////////////// ADD TODOS  ///////////////////////////////////////////////////
+
+const searchInputButton = document.querySelector("#searchInput")
+
+searchInputButton.onclick = () => {
+    searchInputButton.addEventListener("keypress", (event) => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+        }
+    });
+}
+
+const categoriesWrapper = document.querySelector(".all-categories-wrapper")
+let category
+let elementColor
+let verifCategory = false
+
+categoriesWrapper.addEventListener("click", e => {
+    category = e.target.innerText
+    let element = getComputedStyle(e.target)
+    elementColor = element.backgroundColor
+
+    verifCategory = true
+
+    if (!e.target.classList.contains("all-categories-wrapper")) {
+        categoriesWrapper.classList.add('preActive')
+    }
+
+    let categoriesWrapperChild = categoriesWrapper.querySelector(".active");
+
+    if (categoriesWrapperChild != null && !e.target.classList.contains("all-categories-wrapper")) {
+        categoriesWrapperChild.classList.remove('active')
+    }
+
+    if (!e.target.classList.contains("all-categories-wrapper")) {
+        e.target.classList.add("active")
+    }
+
+})
+
+const createTaskButton = document.querySelector("#createTaskButton")
+
+createTaskButton.addEventListener('click', (e) => {
+
+    let search = document.querySelector("#searchInput").value
+
+    let curentData = {"task": search.replace(/<[^>]*>/g, ''), "category": category, "color": elementColor}
+
+    e.preventDefault();
+
+    if ((search !== "") && (verifCategory === true)) {
+
+        let globalNumber
+
+        chrome.storage.sync.get(null, function (data) {
+
+            let number = 0
+
+            let lastTodoNumber = todosGlobalWrapper.lastElementChild.classList[1]
+
+            number = Number(lastTodoNumber) + Number(1)
+
+            globalNumber = number
+
+            chrome.storage.sync.set({[number]: curentData}, function () {
+                todosWrapper.innerHTML += `
+            <div class="todos-wrapper ${number}">
+                <label class="todos-checkbox">
+                    <input class="checkbox_input" type="checkbox">
+                    <svg class="checkbox_check" width="25" height="25">
+                        <polyline points="16 3 7 15 2 10"></polyline>
+                    </svg>
+                </label>
+                <div class="todos-right-wrapper">
+                    <div class="todos-image-wrapper" style="background-color: ${elementColor}">
+                        <img src="image/${category}.png" alt="image">
+                    </div>
+                    <div class="todos-right-main-wrapper">
+                        <h1 class="todos-h1">${curentData.task}</h1>
+                    </div>
+                    <div class="todos-right-color-wrapper" style="background-color: ${elementColor}"></div>
+                </div>
+            </div>
+        `
+            });
+            console.log(data)
+        })
+
+        switchSections("#add-global-wrapper", "#main-global-wrapper")
+
+        setTimeout(() => {
+            scrollToDo(globalNumber)
+        }, 100)
+
+        verifCategory = false
+
+        cleanAddSection()
+
+    }
+
+})
+
+/////////////////////////////////////////////// DELETE TODOS ///////////////////////////////////////////////////
+
+document.addEventListener("click", e => {
+    if (e.target.classList.contains("checkbox_check") || e.target.classList.contains("checkbox_input")) {
+
+        let todosRightWrapper = e.target.parentElement.parentElement.children[1]
+        let parentTodos = todosRightWrapper.parentElement
+        let parentTodosHeight = parentTodos.offsetHeight
+        todosRightWrapper.classList.add("checked")
+        todosRightWrapper.children[2].classList.add("checked")
+
+        let deleteTimeLine = gsap.timeline()
+
+        setTimeout(() => {
+            deleteTimeLine.to(parentTodos, {
+                duration: 0.35,
+                ease: "power4.out",
+                scale: 1.05
+            });
+            deleteTimeLine.to(parentTodos, {
+                duration: 0.7,
+                ease: "power4.out",
+                scale: 0.5,
+                opacity: 0
+            });
+
+            if (todosGlobalWrapper.lastElementChild !== e.target.parentElement.parentElement) {
+
+                deleteTimeLine.to(parentTodos, {
+                    duration: 0.7,
+                    ease: "power4.out",
+                    marginBottom: -parentTodosHeight
+                }, "-=0.35");
+
+            } else {
+
+                deleteTimeLine.to(parentTodos, {
+                    duration: 0.7,
+                    ease: "power4.out",
+                    marginTop: -parentTodosHeight - 15
+                }, "-=0.35");
+
+            }
+
+
+        }, 350)
+
+        let todosCount = e.target.parentElement.parentElement.className.split(' ')
+        chrome.storage.sync.remove(todosCount[1], function () {})
+
+        setTimeout(() => {
+            e.target.parentElement.parentElement.remove()
+        }, 1800)
+
+    }
+})
+
---
+++ js/other.js
@@ -0,0 +1,156 @@
+// Remove scroll bar ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+setTimeout(() => {
+
+    const removeScrollBar = document.createElement('style');
+    removeScrollBar.id = 'remove-scroll-style';
+    removeScrollBar.textContent =
+        'html::-webkit-scrollbar{display:none !important}' +
+        'body::-webkit-scrollbar{display:none !important}';
+    document.getElementsByTagName('body')[0].appendChild(removeScrollBar);
+
+}, 5)
+
+// Gsap ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function animateAllTodos() {
+
+    setTimeout(() => {
+        gsap.from(".todos-wrapper", {
+            duration: 0.5,
+            y: 20,
+            opacity: 0,
+            stagger: 0.15,
+            ease: "power2.inOut",
+        })
+    }, 1)
+
+}
+
+animateAllTodos()
+
+function switchSections(sectionToRemove, sectionToSee) {
+
+    const timeLineSwitchSections = gsap.timeline()
+
+    timeLineSwitchSections.to(
+        sectionToRemove, {opacity: 0, zIndex: 0, duration: 0.5}
+    )
+    timeLineSwitchSections.to(
+        sectionToSee, {opacity: 1, zIndex: 1, duration: 0.5}
+    )
+
+}
+
+// Date ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let actualDay = new Date().getDay()
+let dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+let actualDate = new Date().getDate()
+
+const titleWrapper = document.querySelector(".main-title-wrapper .left-part")
+titleWrapper.innerHTML = `<p>Today's schedule <br>${dayOfTheWeek[actualDay] + " " + actualDate}</p>`
+
+// Switch between sections  ////////////////////////////////////////////////////////////////////////////////////////////
+
+const addTodoButton = document.querySelector("#add-wrapper")
+const backArrow = document.querySelector(".back-arrow-wrapper")
+
+addTodoButton.onclick = () => {
+    switchSections("#main-global-wrapper", "#add-global-wrapper")
+    searchInputButton.focus()
+}
+
+backArrow.onclick = () => {
+    switchSections("#add-global-wrapper", "#main-global-wrapper")
+    setTimeout(() => {
+        animateAllTodos()
+    }, 450)
+
+}
+
+// Clean up the add sections  //////////////////////////////////////////////////////////////////////////////////////////
+
+function cleanAddSection() {
+    setTimeout(() => {
+
+        searchInputButton.value = ""
+        let categoriesWrapperChild = categoriesWrapper.querySelector(".active");
+
+        if (categoriesWrapperChild != null) {
+            categoriesWrapperChild.classList.remove('active')
+        }
+
+        categoriesWrapper.classList.remove("preActive")
+
+    }, 350)
+
+}
+
+createTaskButton.addEventListener("mouseenter", function () {
+
+    if (category === undefined || searchInputButton.value === "") {
+        createTaskButton.style.cursor = "not-allowed"
+    } else {
+        createTaskButton.style.cursor = "pointer"
+    }
+
+})
+
+createTaskButton.addEventListener("mouseleave", function () {
+    createTaskButton.style.cursor = "initial"
+})
+
+// ScrollTo when new to do is added  ///////////////////////////////////////////////////////////////////////////////////
+
+const todosGlobalWrapper = document.querySelector("#todos-global-wrapper")
+
+
+function scrollToDo(toDoToSee) {
+
+    let todosWrapperToSee
+
+    for (let i = 1; i < todosGlobalWrapper.childElementCount; i++) {
+
+        if (todosGlobalWrapper.children[i].classList[1] === toDoToSee.toString()) {
+            todosWrapperToSee = todosGlobalWrapper.children[i]
+
+            todosWrapperToSee.style.opacity = "0"
+
+            let todosWrapperToSeeOffsetTop = todosWrapperToSee.offsetTop
+
+            setTimeout(() => {
+
+                todosGlobalWrapper.scrollTo({
+                    top: todosWrapperToSeeOffsetTop,
+                    left: 0,
+                    behavior: 'instant'
+                });
+
+                setTimeout(() => {
+                    todosWrapperToSee.style.opacity = "1"
+                    gsap.from(todosWrapperToSee, {
+                        duration: 0.5,
+                        scale: 0.5,
+                        opacity: 0,
+                        ease: "power2.inOut"
+                    });
+                }, 700)
+
+                setTimeout(() => {
+                    todosGlobalWrapper.scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth'
+                    });
+                }, 1600)
+
+            }, 300)
+
+            break
+        }
+    }
+
+}
+

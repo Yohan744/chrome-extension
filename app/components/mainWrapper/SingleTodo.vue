561bfd5abf450a2a4500c32713fc2199fb95ba23
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2025-10-16 16:31:27 +0200

    checkbox is almost done

--- app/components/mainWrapper/SingleTodo.vue
+++ app/components/mainWrapper/SingleTodo.vue
@@ -1,20 +1,37 @@
 <template>
-  <div class="todo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, quisquam!</div>
+  <div class="single-todo">
+    <TodoCheckbox />
+    <div class="right-part">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, quisquam!</div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import TodoCheckbox from '~/components/mainWrapper/TodoCheckbox.vue';
+</script>
 
 <style scoped lang="scss">
-  .todo {
+  .single-todo {
     position: relative;
-    padding: 20px;
     height: auto;
     width: 100%;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    border-radius: 10px;
-    background: $color-gray;
+    gap: 10px;
+
+    .right-part {
+      position: relative;
+      padding: 20px;
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      gap: 10px;
+      border-radius: 10px;
+      font-size: 14px;
+      background: rgba($color-gray, 0.4);
+    }
   }
 </style>
---
+++ app/components/mainWrapper/TodoCheckbox.vue
@@ -0,0 +1,55 @@
+<template>
+  <label class="todo-checkbox">
+    <input type="checkbox" />
+    <svg width="25" height="25">
+      <polyline points="16 3 7 15 2 10" />
+    </svg>
+  </label>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+  .todo-checkbox {
+    cursor: pointer;
+    transform: scale(0.8);
+
+    &:hover svg {
+      background: $color-magenta;
+      border: 3px solid $color-magenta;
+    }
+
+    input {
+      width: 0;
+      height: 0;
+      opacity: 0;
+      display: none;
+
+      &:checked + svg {
+        background: $color-magenta;
+        border: 3px solid $color-magenta;
+        stroke-dashoffset: -45;
+      }
+    }
+
+    svg {
+      border: 3px solid $color-light-gray;
+      stroke: white;
+      stroke-dasharray: 23;
+      stroke-dashoffset: -69;
+      stroke-linecap: round;
+      stroke-width: 3px;
+      border-radius: 4px;
+      fill: none;
+      background: transparent;
+      transition:
+        border calc($transition-time * 0.85) $easePower2Out,
+        background calc($transition-time * 0.85) $easePower2Out,
+        stroke-dashoffset calc($transition-time * 0.85) $easePower2Out;
+
+      polyline {
+        transform: scale(0.775) translate(2px, 1px);
+      }
+    }
+  }
+</style>

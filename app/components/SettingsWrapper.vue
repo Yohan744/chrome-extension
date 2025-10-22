c4fa7f625c222c2c086372e506e65ed9ed6d386d
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2025-10-22 23:24:03 +0200

    init settings wrapper

--- app/components/SettingsWrapper.vue
+++ app/components/SettingsWrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="settings-wrapper">
-    <h1>settings wrapper</h1>
+    <h1 class="title">Settings</h1>
   </section>
 </template>
 
@@ -9,15 +9,24 @@
 <style scoped lang="scss">
   #settings-wrapper {
     position: absolute;
+    gap: 30px;
+    padding: 30px 20px calc($footer-height + 20px);
     inset: 0;
     height: 100%;
     width: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-content: center;
+    justify-content: flex-start;
+    align-content: flex-start;
     background: linear-gradient($color-indigo 0, $color-rebecca-purple 0, $color-black 40%);
     pointer-events: none;
     opacity: 0;
+
+    .title {
+      position: relative;
+      font-size: 23px;
+      font-variation-settings: 'wght' 800;
+      color: $color-white;
+    }
   }
 </style>

ee618cbaffc7c1ea163ca0b1bbbb2cdac4bbbb94
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2025-10-31 15:00:41 +0100

    new nuxt config settings and fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     baseURL: './',
     buildAssetsDir: 'nuxt/',
     head: {
-      title: 'Chrome extension',
+      title: 'To do list - Keep it simple',
       htmlAttrs: {
         lang: 'fr'
       },
@@ -31,23 +31,25 @@ export default defineNuxtConfig({
           name: 'viewport',
           content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, viewport-fit=cover'
         },
-        { name: 'description', content: 'TODO' },
+        { name: 'author', content: 'Yohan Quinquis' },
+        { name: 'keywords', content: 'to do, todo, task, list, extension, chrome, firefox, simple' },
+        { name: 'description', content: 'Just a simple to do list extension with a simple design.' },
         { name: 'format-detection', content: 'telephone=no' },
         { name: 'msapplication-TileColor', content: '#ffffff' },
         { name: 'theme-color', content: '#ffffff' },
 
         { property: 'og:type', content: 'website' },
         { property: 'og:url', content: '/' },
-        { property: 'og:title', content: 'TODO' },
-        { property: 'og:description', content: 'TODO' },
+        { property: 'og:title', content: 'To do list - Keep it simple' },
+        { property: 'og:description', content: 'Just a simple to do list extension with a simple design.' },
         { property: 'og:image', content: '/sharing.webp' },
 
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:url', content: '/' },
-        { name: 'twitter:title', content: 'TODO' },
-        { name: 'twitter:description', content: 'TODO' },
+        { name: 'twitter:title', content: 'To do list - Keep it simple' },
+        { name: 'twitter:description', content: 'Just a simple to do list extension with a simple design.' },
         { name: 'twitter:image', content: '/sharing.webp' },
-        { name: 'apple-mobile-web-app-title', content: 'TODO' },
+        { name: 'apple-mobile-web-app-title', content: 'To do list - Keep it simple' },
         { name: 'mobile-web-app-capable', content: 'yes' },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }
       ]
@@ -95,7 +97,8 @@ export default defineNuxtConfig({
         overlay: false
       },
       fs: {
-        strict: false
+        strict: false,
+        allow: ['.']
       },
       watch: {
         usePolling: false,
@@ -103,7 +106,8 @@ export default defineNuxtConfig({
       }
     },
     optimizeDeps: {
-      exclude: ['vue-demi']
+      exclude: ['vue-demi'],
+      include: ['vue', 'vue-router']
     },
 
     plugins: [
@@ -131,6 +135,8 @@ export default defineNuxtConfig({
     },
 
     build: {
+      target: 'es2020',
+      cssCodeSplit: true,
       cssMinify: true,
       ssrManifest: true,
       minify: 'terser',
@@ -139,7 +145,8 @@ export default defineNuxtConfig({
           drop_console: false, // TODO SET BACK WHEN IN PROD
           drop_debugger: false
         }
-      }
+      },
+      commonjsOptions: { transformMixedEsModules: true }
     },
 
     css: {
@@ -188,14 +195,10 @@ export default defineNuxtConfig({
   fonts: {
     families: [
       {
-        name: 'stolzlregular',
+        name: 'sora',
         provider: 'local',
-        src: [
-          './assets/fonts/stolzl_regular.woff2',
-          './assets/fonts/stolzl_regular.woff',
-          './assets/fonts/stolzl_regular.ttf'
-        ],
-        weight: 400,
+        src: ['./assets/fonts/sora.ttf'],
+        weight: '100 900',
         style: 'normal',
         display: 'swap',
         preload: true

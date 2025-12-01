e734504a6a0d3769d3baa78796d471778d81c586
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2025-12-01 23:51:04 +0100

    started implementing icons, work only

--- app/constants/IIcons.ts
+++ app/constants/IIcons.ts
@@ -1,5 +1,17 @@
 export const IIcons = {
+  ///////////////////////////////////////////////////////
+
   briefcase: 'briefcase-business',
+  laptop: 'laptop',
+  filePen: 'file-pen',
+  calendar: 'calendar',
+  chart: 'chart-line',
+  check: 'receipt-text',
+  clock: 'alarm-clock',
+  folder: 'folder-open',
+
+  ///////////////////////////////////////////////////////
+
   cookie: 'cookie',
   utensils: 'utensils',
   dumbbell: 'dumbbell',
